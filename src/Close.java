7de5d6f7b59fca71ff494851a3bba35bc5f1cd6c
Djinnz <ole.gentzsch@gmail.com>
2019-12-13 12:47:24 +0100

    Imported changes

--- src/Close.java
+++ src/Close.java
@@ -1,14 +1,8 @@
 import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 public class Close {
 
