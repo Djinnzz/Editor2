fd323e08e884cb6a5310792f243652ee9e60b54e
Djinnz <ole.gentzsch@gmail.com>
2019-12-13 12:17:34 +0100

    wrote save method

--- src/Save.java
+++ src/Save.java
@@ -1,2 +1,10 @@
+import javax.swing.filechooser.FileSystemView;
+
 public class Save {
+
+    public static String getStandardPath(){
+        return FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+    }
+
+
 }
